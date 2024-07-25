5bc9534a844ca37fa71e42932515dff8bcbaebed
sebcuellar6 <seabasscuellarh@gmail.com>
2024-07-25 12:07:03 -0500

    mapped through forums

--- Moto-Media/src/components/Home.jsx
+++ Moto-Media/src/components/Home.jsx
@@ -1,9 +1,30 @@
 import '../App.css'
 import Carousel from 'react-bootstrap/Carousel';
 import HomeMoto from '../photos/HomeMoto.drawio.png'
+import Container from 'react-bootstrap/Container'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Card from 'react-bootstrap/Card'
 
 
 export default function Home () {
+
+  const [forums, setForums] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+
+        const response =  await axios.get('http://localhost:8000/forumposts/')
+        console.log(response.data)
+        setForums(response.data)
+      }catch (error) {
+        console.log(error)
+      }
+    }
+    getData()
+  }, [])
+
 
   return(
     <div className='heroPage'>
@@ -46,6 +67,22 @@ export default function Home () {
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
+      <Container className='home-card'>
+        {forums.map((forum) => (
+
+          <Card border="primary" style={{ width: '18rem' }} key={forum.id}>
+          <Card.Header>{forum.username}</Card.Header>
+          <Card.Body>
+            <Card.Title>{forum.title}</Card.Title>
+            <Card.Text>
+              {forum.body}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        ))}
+
+      </Container>
       
     </div> 
   )
